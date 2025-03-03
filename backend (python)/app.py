b80ec77c47f9ec4b9e0bf6b43475ecddd4edb14b
dcantau <dcantau@pfr-dsk22.planfor.local>
2025-03-03 19:39:50 +0100

    V0.2

--- backend (python)/app.py
+++ backend (python)/app.py
@@ -9,8 +9,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Virement(db.Model):
-    __tablename__ = 'virement'
-    id = db.Column(db.String(5), primary_key=True, autoincrement=True)
+    __tablename__ = 'virement_temp'
+    id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(50))
     date_prod = db.Column(db.Date)
     montant = db.Column(db.Integer, nullable=False)
@@ -49,17 +49,23 @@ def modify_virement(id):
         return jsonify({'message': 'Virement modified successfully'}), 200
     else:
         return jsonify({'message': 'Virement not found'}), 404
+
 @app.route('/api/virements', methods=['POST'])
 def add_virement():
-    id = request.json.get('id')
     type = request.json.get('type')
     date_prod = request.json.get('date_prod')
     montant = request.json.get('montant')
     description = request.json.get('description')
-    virement = Virement(id=id, type=type, date_prod=date_prod, montant=montant, description=description)
+    virement = Virement(type=type, date_prod=date_prod, montant=montant, description=description)
     db.session.add(virement)
     db.session.commit()
-    return jsonify({'message': 'Virement added successfully'}), 201
+    return jsonify({
+        'id': virement.id,
+        'type': virement.type,
+        'date_prod': virement.date_prod,
+        'montant': virement.montant,
+        'description': virement.description
+    }), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
