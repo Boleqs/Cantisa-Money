aadb8790d2116f1c5133051f7ea1efcc6979ee5e
Boleqs <boleqs@gmail.com>
2025-03-05 00:21:10 +0100

    sql v0.3

--- backend (python)/db_definition.sql
+++ backend (python)/db_definition.sql
@@ -1,6 +1,5 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-
 -- Partie gestion des users / sécurité de l'app
 
 CREATE TABLE users (
@@ -49,7 +48,8 @@ CREATE TABLE accounts (
     is_virtual   BOOLEAN DEFAULT FALSE NOT NULL,
     is_hidden    BOOLEAN DEFAULT FALSE NOT NULL,
     code         VARCHAR(64) NULL,
-	created_at DATE DEFAULT CURRENT_DATE,
+	created_at   DATE DEFAULT CURRENT_DATE,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     -- clé primaire
     PRIMARY KEY (user_id, id),
@@ -143,6 +143,15 @@ CREATE TABLE tags (
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
+CREATE TABLE tags_on_split (
+    split_id    CHAR(36) NOT NULL,
+    tag_id      CHAR(36) NOT NULL,
+    -- clé unique
+    UNIQUE (split_id, tag_id),
+    -- clés étrangères
+    FOREIGN KEY (split_id) REFERENCES splits (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
 
 -- Partie gestion Finances
 
@@ -207,6 +216,8 @@ CREATE TABLE budget_accounts (
     budget_id       CHAR(36) NOT NULL,
     account_id      CHAR(36) NOT NULL,
 
+    -- clé unique
+    UNIQUE (budget_id, account_id),
     -- Clés étrangères
     FOREIGN KEY (budget_id) REFERENCES  budgets (id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES accounts (name) ON DELETE CASCADE
@@ -217,23 +228,60 @@ CREATE TABLE budget_categories (
     budget_id       CHAR(36) NOT NULL,
     category_id      CHAR(36) NOT NULL,
 
+    -- clé unique
+    UNIQUE (budget_id, category_id),
     -- Clés étrangères
     FOREIGN KEY (budget_id) REFERENCES  budgets (id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
 
+CREATE TABLE budget_tags (
+    budget_id    CHAR(36) NOT NULL,
+    tag_id      CHAR(36) NOT NULL,
+    -- clé unique
+    UNIQUE (budget_id, tag_id),
+    -- clés étrangères
+    FOREIGN KEY (budget_id) REFERENCES budgets (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
+
 
 -- Triggers
 
+-- Auto update budget spent
+
 CREATE OR REPLACE FUNCTION update_budget_spent()
 RETURNS TRIGGER AS $$
+DECLARE
+    total_spent INT;
 BEGIN
-    -- Met à jour le budget en additionnant toutes les transactions liées
+    -- Calculer le total des transactions liées aux comptes associés au budget
+    SELECT COALESCE(SUM(s.quantity), 0) INTO total_spent
+    FROM splits s
+    WHERE s.account_id IN (
+        SELECT account_id FROM budget_accounts WHERE budget_id = NEW.id
+    );
+
+    -- Ajouter les transactions liées aux catégories associées au budget
+    SELECT COALESCE(total_spent + SUM(s.quantity), total_spent) INTO total_spent
+    FROM splits s
+    JOIN transactions t ON s.tx_id = t.id
+    WHERE t.category_id IN (
+        SELECT category_id FROM budget_categories WHERE budget_id = NEW.id
+    );
+
+    -- Ajouter les transactions liées aux tags associés au budget
+    SELECT COALESCE(total_spent + SUM(s.quantity), total_spent) INTO total_spent
+    FROM splits s
+    JOIN tags_on_split ts ON s.id = ts.split_id
+    WHERE ts.tag_id IN (
+        SELECT tag_id FROM budget_tags WHERE budget_id = NEW.id
+    );
+
+    -- Mettre à jour le montant dépensé dans le budget
     UPDATE budgets
-    SET amount_spent = (
-        SELECT SUM(SELECT ) FROM budgets
-    )
-    WHERE id = NEW.budget_id;
+    SET amount_spent = total_spent
+    WHERE id = NEW.id;
 
     RETURN NEW;
 END;
@@ -243,3 +291,28 @@ CREATE TRIGGER trg_log_budget_changes
 BEFORE UPDATE ON budgets
 FOR EACH ROW
 EXECUTE FUNCTION update_budget_spent();
+
+-- Auto update fields updated_at
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_timestamp
+BEFORE UPDATE ON accounts
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+CREATE TRIGGER trg_update_timestamp
+BEFORE UPDATE ON budgets
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+CREATE TRIGGER trg_update_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
